0c5746a89e56df8f5af2b947dcbaa71c3a4a0394
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-14 10:22:05 +0800

    added required to fields, except phone number (#3)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import { StaticImage } from "gatsby-plugin-image"
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
 export default function Example() {
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false)
 
-  const success_message = (event) => {
-    event.preventDefault();
-    setSuccess(true);
+  const success_message = event => {
+    event.preventDefault()
+    setSuccess(true)
   }
 
   const headings = [
     {
       h1_heading: "Get in touch",
-      h1_desc: "If you’re looking for assistance with a trademark reach out and we can get you connect you with someone who can help.",
+      h1_desc:
+        "If you’re looking for assistance with a trademark reach out and we can get you connect you with someone who can help.",
       h3_heading: "Describe what you need",
-      h3_desc: "Give us an overview of who you are and what you need help with someone will be in touch within a business day or two.",
-      success_message: "Your message has been received and we will be in touch shortly.",
+      h3_desc:
+        "Give us an overview of who you are and what you need help with someone will be in touch within a business day or two.",
+      success_message:
+        "Your message has been received and we will be in touch shortly.",
     },
   ]
 
@@ -49,16 +52,26 @@ export default function Example() {
             <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
               {headings.map(item => (
                 <>
-                  <h1 className="text-4xl font-bold tracking-tight text-zinc-900 font-sans sm:text-5xl lg:text-6xl"> {item.h1_heading} </h1>
-                  <p className="mt-6 text-lg text-zinc-400 font-sans max-w-3xl"> {item.h1_desc} </p>
+                  <h1 className="text-4xl font-bold tracking-tight text-zinc-900 font-sans sm:text-5xl lg:text-6xl">
+                    {item.h1_heading}
+                  </h1>
+                  <p className="mt-6 text-lg text-zinc-400 font-sans max-w-3xl">
+                    {item.h1_desc}
+                  </p>
                 </>
               ))}
             </div>
           </div>
         </div>
 
-        <section className="relative bg-stone-50 py-4 " aria-labelledby="contact-heading">
-          <div className="absolute w-full h-1/2 bg-warm-gray-50" aria-hidden="true" />
+        <section
+          className="relative bg-stone-50 py-4 "
+          aria-labelledby="contact-heading"
+        >
+          <div
+            className="absolute w-full h-1/2 bg-warm-gray-50"
+            aria-hidden="true"
+          />
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <svg
               className="absolute z-0 top-0 right-0 transform -translate-y-16 translate-x-1/2 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
@@ -77,10 +90,21 @@ export default function Example() {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-zinc-200" fill="currentColor" />
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-zinc-200"
+                    fill="currentColor"
+                  />
                 </pattern>
               </defs>
-              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+              <rect
+                width={404}
+                height={384}
+                fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+              />
             </svg>
           </div>
           <div className="max-w-7xl pb-3 mx-auto px-4 sm:px-6 lg:px-8">
@@ -122,17 +146,31 @@ export default function Example() {
                   </div>
                   {headings.map(item => (
                     <>
-                      <h3 className="text-lg font-normal text-stone-100"> {item.h3_heading} </h3>
-                      <p className="text-base text-stone-100 font-sans max-w-3xl"> {item.h3_desc} </p>
+                      <h3 className="text-lg font-normal text-stone-100">
+                        {item.h3_heading}
+                      </h3>
+                      <p className="text-base text-stone-100 font-sans max-w-3xl">
+                        {item.h3_desc}
+                      </p>
                     </>
                   ))}
                 </div>
 
                 <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12 shadow-md">
-                  <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
-                  <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                  <h3 className="text-lg font-medium text-warm-gray-900">
+                    Send us a message
+                  </h3>
+                  <form
+                    action="#"
+                    onSubmit={success_message}
+                    method="POST"
+                    className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+                  >
                     <div>
-                      <label htmlFor="first-name" className="block text-sm font-medium font-sans text-warm-gray-900">
+                      <label
+                        htmlFor="first-name"
+                        className="block text-sm font-medium font-sans text-warm-gray-900"
+                      >
                         First name
                       </label>
                       <div className="mt-1">
@@ -142,11 +180,15 @@ export default function Example() {
                           id="first-name"
                           autoComplete="given-name"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 font-sans border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          required
                         />
                       </div>
                     </div>
                     <div>
-                      <label htmlFor="last-name" className="block text-sm font-medium font-sans text-warm-gray-900">
+                      <label
+                        htmlFor="last-name"
+                        className="block text-sm font-medium font-sans text-warm-gray-900"
+                      >
                         Last name
                       </label>
                       <div className="mt-1">
@@ -156,11 +198,15 @@ export default function Example() {
                           id="last-name"
                           autoComplete="family-name"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 font-sans border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          required
                         />
                       </div>
                     </div>
                     <div>
-                      <label htmlFor="email" className="block text-sm font-medium font-sans text-warm-gray-900">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium font-sans text-warm-gray-900"
+                      >
                         Email
                       </label>
                       <div className="mt-1">
@@ -170,15 +216,22 @@ export default function Example() {
                           type="email"
                           autoComplete="email"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 font-sans border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          required
                         />
                       </div>
                     </div>
                     <div>
                       <div className="flex justify-between">
-                        <label htmlFor="phone" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        <label
+                          htmlFor="phone"
+                          className="block text-sm font-medium font-sans text-warm-gray-900"
+                        >
                           Phone
                         </label>
-                        <span id="phone-optional" className="text-sm font-sans text-zinc-400">
+                        <span
+                          id="phone-optional"
+                          className="text-sm font-sans text-zinc-400"
+                        >
                           Optional
                         </span>
                       </div>
@@ -194,7 +247,10 @@ export default function Example() {
                       </div>
                     </div>
                     <div className="sm:col-span-2">
-                      <label htmlFor="subject" className="block text-sm font-medium font-sans text-warm-gray-900">
+                      <label
+                        htmlFor="subject"
+                        className="block text-sm font-medium font-sans text-warm-gray-900"
+                      >
                         Subject
                       </label>
                       <div className="mt-1">
@@ -203,15 +259,22 @@ export default function Example() {
                           name="subject"
                           id="subject"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 font-sans border-2 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          required
                         />
                       </div>
                     </div>
                     <div className="sm:col-span-2">
                       <div className="flex justify-between">
-                        <label htmlFor="message" className="block text-sm font-medium font-sans text-warm-gray-900">
+                        <label
+                          htmlFor="message"
+                          className="block text-sm font-medium font-sans text-warm-gray-900"
+                        >
                           Message
                         </label>
-                        <span id="message-max" className="text-sm font-sans text-zinc-400">
+                        <span
+                          id="message-max"
+                          className="text-sm font-sans text-zinc-400"
+                        >
                           Max. 500 characters
                         </span>
                       </div>
@@ -222,23 +285,26 @@ export default function Example() {
                           rows={4}
                           className="py-3 px-4 block w-full shadow text-warm-gray-900 font-sans border-2 focus:ring-teal-500 focus:border-teal-500 border border-warm-gray-300 rounded-md"
                           aria-describedby="message-max"
-                          defaultValue={''}
+                          defaultValue={""}
+                          required
                         />
                       </div>
                     </div>
                     <div className="sm:col-span-2 sm:flex sm:justify-end">
                       <button
-                        onClick={success_message}
+                        type="submit"
                         className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium font-sans text-white bg-teal-500 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
                       >
                         Submit
                       </button>
                     </div>
-                    {success ? (
-                      headings.map(item => (
-                        <p className="text-sm w-full font-bold tracking-tight text-zinc-400 font-sans sm:text-sm"> {item.success_message} </p>
-                      ))
-                    ) : ""}
+                    {success
+                      ? headings.map(item => (
+                          <p className="text-sm w-full font-bold tracking-tight text-zinc-400 font-sans sm:text-sm">
+                            {item.success_message}
+                          </p>
+                        ))
+                      : ""}
                   </form>
                 </div>
               </div>
