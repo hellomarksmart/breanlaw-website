3551815709166956450ebfe152a57ea1c413b2ab
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-02-01 13:11:29 +0800

    nodemailer added (#23)

--- form.js
+++ form.js
@@ -1,57 +1,24 @@
-const express = require("express");
 const nodemailer = require("nodemailer");
-const app = express();
-const cors = require("cors");
-require("dotenv").config();
+
 const contactAddress = "docket@remarkable.legal"
 
-// middleware
-app.use(express.json());
-app.use(cors());
-
-let transporter = nodemailer.createTransport({
+const smtpTransport = nodemailer.createTransport({
   host: 'smtp.migadu.com',
   port: 587,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.WORD,
+    user: 'docket@remarkable.legal',
+    pass: '4h7Q8n--TCgMb'
   }
 });
 
-transporter.verify((err, success) => {
-  err
-    ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success} ===`);
-});
-
-app.post("/send", function (req, res) {
-  let mailOptions = {
-    from: [contactAddress],
+async function run() {
+  let sendResult = await smtpTransport.sendMail({
+    from: "docket@remarkable.legal",
     to: [contactAddress],
-    subject: `Email from Breanlaw: ${req.body.mailerState.subject}`,
-    text: `${req.body.mailerState.message}`,
-    html: `<p>Name: ${req.body.mailerState.FName} ${req.body.mailerState.LName}</p>
-    <p>Email: ${req.body.mailerState.email}</p>
-    <p>Phone Number: ${req.body.mailerState.phone}</p>
-    <p>Message: ${req.body.mailerState.message}</p>
-    `
-  };
+    subject: "Testing - please disregard",
+    html: "[No message]",
+  })
+  console.log(sendResult);
+}
 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      res.json({
-        status: "fail",
-      });
-    } else {
-      console.log("== Message Sent ==");
-      res.json({
-        status: "success",
-      });
-    }
-  });
-});
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+run().catch(err => console.error(err));
